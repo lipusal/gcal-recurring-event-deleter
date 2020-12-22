26afbcb9288ae7ac3ed7ef5063917b56fa9c9fbc
Juan Li Puma <juanlipuma94@gmail.com>
2020-12-21 22:52:35 -0500

    Remove limit form list_events

--- lib/commands/delete-range.rb
+++ lib/commands/delete-range.rb
@@ -40,17 +40,14 @@ module Commands
 
     desc 'list_events', 'List upcoming recurring events in the specified calendar'
     method_option :calendar_id, type: :string, required: true
-    method_option :limit, type: :numeric, default: 50
     def list_events
       calendar = Calendar::CalendarService.new
       calendar.authorization = user_credentials_for(Calendar::AUTH_CALENDAR_EVENTS_READONLY)
 
       page_token = nil
-      limit = options[:limit]
       now = Time.now.iso8601
       begin
         result = calendar.list_events(options[:calendar_id],
-                                      max_results: limit,
                                       single_events: false,
                                       order_by: 'startTime',
                                       time_min: now,
@@ -61,13 +58,12 @@ module Commands
           time = event.start.date_time || event.start.date
           say "#{time}, #{event.summary}"
         end
-        limit -= result.items.length
         page_token = if result.next_page_token
           result.next_page_token
         else
           nil
         end
-      end while !page_token.nil? && limit > 0
+      end while !page_token.nil?
     end
 
     private
